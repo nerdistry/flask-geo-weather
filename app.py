1189881ec1e87f36e680d95b7872ca1502c4a1d8
Arabella Fanisheba <arabella.mutende@strathmore.edu>
2023-09-23 16:53:34 +0300

    Just started app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello world")
+from flask import Flask, session, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'your-secret-key'
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
